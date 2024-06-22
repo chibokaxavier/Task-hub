8b865f03dcc06c7bf4002c2d6f031cebdada732d
manlikexavy <chibokaxavier@gmail.com>
2024-06-22 21:44:56 +0100

    Working on the UI of the single task page

--- src/app/tasks/[id]/page.tsx
+++ src/app/tasks/[id]/page.tsx
@@ -29,12 +29,27 @@ const page = ({ params }: { params: { id: string } }) => {
   }, [task]);
   return (
     <>
-      <div className="text-black">
-        {task ? (
-          <div className="text-black">{task.name}</div>
-        ) : (
-          <p>Loading...</p>
-        )}
+      <div className="justify-center items-center flex">
+        <div className="bg-black h-fit w-fit mb-10 flex-col justify-center p-[60px]">
+          <div className="justify-center items-center ">
+            <p className=" text-white text-center pb-4 text-2xl">Edit Task</p>
+            <div className="flex space-x-3">
+              <input
+                type="text"
+                className="outline-none border-0 text-black px-2"
+                placeholder="e.g Wash dishes"
+              />{" "}
+              <button
+                type="submit"
+                className="rounded-sm bg-blue-500 p-2 text-black font-bold"
+              >
+                {" "}
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="text-black">{task?.name}</div>
       </div>
     </>
   );
