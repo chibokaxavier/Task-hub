c3a88161d6545e24af9111220a2b651f281b8729
manlikexavy <chibokaxavier@gmail.com>
2024-06-24 16:08:59 +0100

    UPDATE FUNCTIONALITY WORKING, UP NEXT IS TO MAKE IT RESPONSIVE

--- src/app/tasks/[id]/page.tsx
+++ src/app/tasks/[id]/page.tsx
@@ -7,6 +7,32 @@ type Task = {
 };
 const page = ({ params }: { params: { id: string } }) => {
   const [task, setTask] = useState<Task | null>(null);
+  const [name, setName] = useState("");
+  const [completed, setCompleted] = useState(false);
+  const updateTask = async () => {
+    if (!task) {
+      return;
+    }
+    const updatedTask = {
+      ...task,
+      name,
+      completed,
+    };
+    try {
+      const response = await fetch(
+        `http://localhost:3001/api/v1/tasks/${task._id.toString()}`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(updatedTask),
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
     const fetchTask = async () => {
       try {
@@ -18,15 +44,14 @@ const page = ({ params }: { params: { id: string } }) => {
         }
         const result = await response.json();
         setTask(result.task);
+        setName(result.task.name);
+        setCompleted(result.task.completed);
       } catch (error) {
         console.log(error);
       }
     };
     fetchTask();
   }, []);
-  useEffect(() => {
-    console.log(task);
-  }, [task]);
   return (
     <>
       <main className="flex justify-center items-center mt-10">
@@ -40,20 +65,28 @@ const page = ({ params }: { params: { id: string } }) => {
             <p>Name</p>
             <input
               type="text"
-
-              value={task?.name.toString()}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="bg-gray-200 outline-none text-black w-[350px] px-4 border-none"
             />
           </div>
           <div className="flex space-x-9">
             <p>Completed</p>
-            <input type="checkbox" name="" id="" />
+            <input
+              type="checkbox"
+              name=""
+              id=""
+              checked={completed}
+              onChange={(e) => setCompleted(e.target.checked)}
+            />
           </div>
-        
-            <button className=" w-full py-2 bg-blue-700 text-white">
-              Submit
-            </button>
-          
+
+          <button
+            onClick={updateTask}
+            className=" w-full py-2 bg-blue-700 text-white"
+          >
+            Submit
+          </button>
         </div>
       </main>
     </>
